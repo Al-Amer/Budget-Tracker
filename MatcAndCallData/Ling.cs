78dbcc27ae57fcb0619e05d1b44b46dab95ee0d3
ameralmonajed <a.munajjed@icloud.com>
2025-11-26 23:42:02 +0100

    wahil loop with Register ans login it is ready

--- MatcAndCallData/Ling.cs
+++ MatcAndCallData/Ling.cs
@@ -3,6 +3,17 @@ using System;
 public class Ling
 {
     // check data
+    public string folder = "Data";
+    public void checkDir()
+    {
+        if (!Directory.Exists(folder)){
+            Console.WriteLine($"Directory: {folder} it is nut exists");
+        }else if (Directory.Exists(folder))
+        {
+            Console.WriteLine(folder);
+        }
+        
+    }
 
     // comparison data with name and password return false or true 
 
